0152235651af5ca8384c079022a62b558233513c
YoungWoong Jun <ywj7373@gmail.com>
2020-12-14 18:58:13 +0900

    change path

--- prepare_data.py
+++ prepare_data.py
@@ -18,19 +18,19 @@ def load_mx_rec(rec_path):
     save_path = rec_path / 'imgs'
     if not save_path.exists():
         save_path.mkdir()
-        imgrec = mx.recordio.MXIndexedRecordIO(str(rec_path / 'train.idx'), str(rec_path / 'train.rec'), 'r')
-        img_info = imgrec.read_idx(0)
-        header, _ = mx.recordio.unpack(img_info)
-        max_idx = int(header.label[0])
-        for idx in tqdm(range(1, max_idx)):
-            img_info = imgrec.read_idx(idx)
-            header, img = mx.recordio.unpack_img(img_info)
-            label = int(header.label)
-            img = Image.fromarray(img)
-            label_path = save_path / str(label)
-            if not label_path.exists():
-                label_path.mkdir()
-            img.save(label_path / '{}.jpg'.format(idx), quality=95)
+    imgrec = mx.recordio.MXIndexedRecordIO(str(rec_path / 'train.idx'), str(rec_path / 'train.rec'), 'r')
+    img_info = imgrec.read_idx(0)
+    header, _ = mx.recordio.unpack(img_info)
+    max_idx = int(header.label[0])
+    for idx in tqdm(range(1, max_idx)):
+        img_info = imgrec.read_idx(idx)
+        header, img = mx.recordio.unpack_img(img_info)
+        label = int(header.label)
+        img = Image.fromarray(img)
+        label_path = save_path / str(label)
+        if not label_path.exists():
+            label_path.mkdir()
+        img.save(label_path / '{}.jpg'.format(idx), quality=95)
 
 
 def load_bin(path, rootdir, transform, image_size=[112, 112]):
